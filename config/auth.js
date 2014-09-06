1f95c16ede67753015775c0412ca5b8d92796da6
vivek <viveks1422@gmail.com>
2014-09-06 13:21:21 +0530

    [oauth fix]

--- config/auth.js
+++ config/auth.js
@@ -5,13 +5,13 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID' 		: '1504720869766777', // your App ID
 		'clientSecret' 	: 'e420703c6df278992927f74e9bb15151', // your App Secret
-		'callbackURL' 	: 'http://localhost:3000/auth/facebook/callback'
+		'callbackURL' 	: 'http://localhost:8000/auth/facebook/callback'
 	},
 
 	'twitterAuth' : {
 		'consumerKey' 		: 'TbZq8GL9msY726ljx4T2CaMuf',
 		'consumerSecret' 	: 'gfAN9Ny8bICdCAGl5yO3MEGQNKzEg8uYFR3K97iAWmhuOkhaTM',
-		'callbackURL' 		: 'http://localhost:3000/auth/twitter/callback'
+		'callbackURL' 		: 'http://localhost:8000/auth/twitter/callback'
 	},
 
 	'googleAuth' : {
