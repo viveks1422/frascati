19931aa7bc286f69cb8999f6a805f92a8c24e39a
vivek <viveks1422@gmail.com>
2014-09-06 18:31:49 +0530

    [registration email]

--- models/user.js
+++ models/user.js
@@ -3,6 +3,8 @@ var mongoose = require('mongoose');
 var Schema=mongoose.Schema;
 var bcrypt   = require('bcrypt-nodejs');
 var Role=require('../models/role');
+// to send email notification
+var nodemailer = require('nodemailer');
 
 // define the schema for our user model
 var userSchema = mongoose.Schema({
@@ -77,6 +79,34 @@ userSchema.pre('save', function(next){
     // to find user role 
     this.role="user";
     next();
+});
+// registration email -------------------------------------
+// create reusable transporter object using SMTP transport
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'vivekshine22@gmail.com',
+        pass: 'vteam123'
+    }
+});
+// callback to send registration email
+userSchema.post('save', function(data){
+    // setup e-mail data with unicode symbols
+    var mailOptions = {
+        from: 'Fabio ✔ <admin@eaemaquinas.com>', // sender address
+        to: data.local.email, // list of receivers
+        subject: 'Eaemaquinas :: Registation sucessfull ✔', // Subject line
+        text: 'Hello Customer', // plaintext body
+        html: '<b>Registration successful ✔</b>' // html body
+    };
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            console.log(error);
+        }else{
+            console.log('Message sent: ' + info.response);
+        }
+    });
 });
 // create the model for users and expose it to our app
 module.exports = mongoose.model('User', userSchema);
